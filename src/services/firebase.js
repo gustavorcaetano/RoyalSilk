8d4d642b506476d32dc28f8e86f1459d42d1b787
gustavorcaetano <gusramoscaetano@gmail.com>
2026-03-16 19:40:47 -0300

    Criação do 'manifest.json' e do  'sw.js'

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY", 
-  authDomain: "royal-silk.firebaseapp.com",
-  projectId: "royal-silk",
-  storageBucket: "royal-silk.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyCINumlkRcOqxNPZUr-Gp8vuMDVa1BnQ-I",
+  authDomain: "royal-silk-cosmetics.firebaseapp.com",
+  projectId: "royal-silk-cosmetics",
+  storageBucket: "royal-silk-cosmetics.firebasestorage.app",
+  messagingSenderId: "1072485065771",
+  appId: "1:1072485065771:web:e2e747a4208e23ade6f66e",
+  measurementId: "G-GJXCP2TYXR"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const db = getFirestore(app); // Exportando o banco de dados
+export default app;
